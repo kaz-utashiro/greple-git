99296ef1f8356cc084470d75b63339977f7f41d2
Kazumasa Utashiro <kaz@utashiro.com>
2021-10-21 21:19:20 +0900

    Minimum update.

--- lib/App/Greple/git.pm
+++ lib/App/Greple/git.pm
@@ -1,39 +1,38 @@
 package App::Greple::git;
-use 5.008001;
-use strict;
+
+use 5.014;
 use warnings;
 
 our $VERSION = "0.01";
 
-
-
 1;
+
 __END__
 
 =encoding utf-8
 
 =head1 NAME
 
-App::Greple::git - It's new $module
+git - Greple module for git
 
 =head1 SYNOPSIS
 
-    use App::Greple::git;
+    greple -Mgit ...
 
 =head1 DESCRIPTION
 
-App::Greple::git is ...
-
-=head1 LICENSE
-
-Copyright (C) Kazumasa Utashiro.
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
+App::Greple::git is a greple module to handle git output.
 
 =head1 AUTHOR
 
-Kazumasa Utashiro E<lt>kaz@utashiro.comE<gt>
+Kazumasa Utashiro
+
+=head1 LICENSE
+
+Copyright 2021 Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
 
 =cut
 
